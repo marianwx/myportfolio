dae6f178a5f3060e5c5b3c1e49f55f8c38eedbe9
marianwx <maransmk69@gmail.com>
2023-12-02 14:39:49 +0200

    .

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+        <header>
+
+            <nav class="flex justify-evenly text-lg">
+                <NuxtLink to="/">My Portfolio</NuxtLink>
+                <div class="flex gap-7">
+                    <NuxtLink to="/">Home</NuxtLink>
+                    <NuxtLink to="/about">About</NuxtLink>
+                    <NuxtLink to="/">Skills</NuxtLink>
+                    <NuxtLink to="/">Projects</NuxtLink>
+                    <NuxtLink to="/">Contact</NuxtLink>
+                </div>
+                <button class="">example</button>
+            </nav>
+
+        </header>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
