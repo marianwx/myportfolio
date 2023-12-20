3f935ca973bb05c237982acf4c3c5e0ef67d5e0c
marianwx <maransmk69@gmail.com>
2023-12-20 15:29:58 +0200

    update

--- portfolio/components/portfolio.vue
+++ portfolio/components/portfolio.vue
@@ -1,63 +1,19 @@
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
- 
- <div class="group cursor-pointer relative">
-   <img
-     src="https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw1fHxuYXR1cmV8ZW58MHwwfHx8MTY5NDA5OTcyOXww&ixlib=rb-4.0.3&q=80&w=1080"
-     alt="Image 1"
-     class="w-full h-48 object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
-   />
-   <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-     <button class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors">
-       View
-     </button>
-   </div>
- </div>
 
- <div class="group cursor-pointer relative">
-   <img
-     src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw5fHxuYXR1cmV8ZW58MHwwfHx8MTY5NDA5OTcyOXww&ixlib=rb-4.0.3&q=80&w=1080"
-     alt="Image 1"
-     class="w-full h-48 object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
-   />
-   <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-     <button class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors">
-       View
-     </button>
-   </div>
- </div>
-
- <div class="group cursor-pointer relative">
-   <img
-     src="https://images.unsplash.com/photo-1501854140801-50d01698950b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxMHx8bmF0dXJlfGVufDB8MHx8fDE2OTQwOTk3Mjl8MA&ixlib=rb-4.0.3&q=80&w=1080"
-     alt="Image 1"
-     class="w-full h-48 object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
-   />
-   <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-     <button class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors">
-       View
-     </button>
-   </div>
- </div>
-
- <div class="group cursor-pointer relative">
-   <img
-     src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwyfHxuYXR1cmV8ZW58MHwwfHx8MTY5NDA5OTcyOXww&ixlib=rb-4.0.3&q=80&w=1080"
-     alt="Image 1"
-     class="w-full h-48 object-cover rounded-lg transition-transform transform scale-100 group-hover:scale-105"
-   />
-   <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-     <button class="bg-white text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-200 transition-colors">
-       View
-     </button>
-   </div>
- </div>
-
-
-
-
-
- <!-- Repeat this div for each image -->
+<div class="grid lg:grid-cols-2 justify-center md:grid-cols-2 gap-4">
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
+    </div>
 </div>
+
 
 </template>
